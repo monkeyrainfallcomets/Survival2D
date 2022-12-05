3f52c62ebc0d88a3f94c25062c602e311187555d
Jack Shea <jack20shea20@gmail.com>
2022-12-05 18:03:53 -0500

    init commit

---
+++ Assets/Scripts/WorldGen/WorldGeneration.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGeneration : MonoBehaviour
+{
+    WorldTemplate[] worldTemplates;
+    void Start()
+    {
+
+    }
+
+
+}
